a9a18cc6213b8ad648b095d558d8bfd32b5f0a32
smallfawn <860562056@qq.com>
2024-12-20 09:39:28 +0800

    Update HomeController.php

--- dujiaoka/app/Http/Controllers/Home/HomeController.php
+++ dujiaoka/app/Http/Controllers/Home/HomeController.php
@@ -67,7 +67,7 @@ class HomeController extends BaseController
             if (!$goods) {
                 // 商品不存在，返回JSON响应
                 return view('home.error', [
-                    'message' => '商品不存在',
+                    'message' => '商品不存在,将在2s后跳转到首页',
                     'redirectUrl' => url('/'),
                     'delay' => 2000, // 2秒后跳转
                 ]);
